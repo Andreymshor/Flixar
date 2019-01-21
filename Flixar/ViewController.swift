8d7244e27b85af184e846864a6ad8babf341a5f7
Andrey Shor <andreyshor@andreys-imac.local>
2019-01-21 11:28:07 -0500

    Initial Commit

---
+++ Flixar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flixar
+//
+//  Created by Andrey Shor on 1/21/19.
+//  Copyright © 2019 Andrey Shor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
