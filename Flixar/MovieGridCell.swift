e6d2b320d828f4183e4470032550d48154437e02
Andrey Shor <andreyshor@andreys-imac.local>
2019-01-27 16:35:35 -0500

    Flixer Final

---
+++ Flixar/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixar
+//
+//  Created by Andrey Shor on 1/27/19.
+//  Copyright © 2019 Andrey Shor. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
